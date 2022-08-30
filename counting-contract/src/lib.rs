1bbb2122d100d74d6b41e969de3363edbbc69cfe
Bartłomiej Kuras <bart.k@confio.gmbh>
2022-08-30 15:44:07 +0200

    Lesson 4 assignment - additional test

--- counting-contract/src/lib.rs
+++ counting-contract/src/lib.rs
@@ -27,6 +27,7 @@ pub fn query(_deps: Deps, _env: Env, msg: msg::QueryMsg) -> StdResult<Binary> {
 
     match msg {
         Value {} => to_binary(&query::value()),
+        Incremented { value } => to_binary(&query::incremented(value)),
     }
 }
 
@@ -66,5 +67,30 @@ mod test {
             .unwrap();
 
         assert_eq!(resp, ValueResp { value: 0 });
+    }
+
+    #[test]
+    fn query_incremented() {
+        let mut app = App::default();
+
+        let contract_id = app.store_code(counting_contract());
+
+        let contract_addr = app
+            .instantiate_contract(
+                contract_id,
+                Addr::unchecked("sender"),
+                &Empty {},
+                &[],
+                "Counting contract",
+                None,
+            )
+            .unwrap();
+
+        let resp: ValueResp = app
+            .wrap()
+            .query_wasm_smart(contract_addr, &QueryMsg::Incremented { value: 1 })
+            .unwrap();
+
+        assert_eq!(resp, ValueResp { value: 2 });
     }
 }
