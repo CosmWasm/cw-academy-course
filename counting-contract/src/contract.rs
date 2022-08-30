5426831405bc9c91f4b6ced5ccd2bf27f6787809
Bartłomiej Kuras <bart.k@confio.gmbh>
2022-08-30 15:37:53 +0200

    Lesson 4 - tests

--- counting-contract/src/contract.rs
+++ counting-contract/src/contract.rs
@@ -4,8 +4,4 @@ pub mod query {
     pub fn value() -> ValueResp {
         ValueResp { value: 0 }
     }
-
-    pub fn incremented(value: u64) -> ValueResp {
-        ValueResp { value: value + 1 }
-    }
 }
--- counting-contract/src/lib.rs
+++ counting-contract/src/lib.rs
@@ -27,6 +27,44 @@ pub fn query(_deps: Deps, _env: Env, msg: msg::QueryMsg) -> StdResult<Binary> {
 
     match msg {
         Value {} => to_binary(&query::value()),
-        Incremented { value } => to_binary(&query::incremented(value)),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use cosmwasm_std::{Addr, Empty};
+    use cw_multi_test::{App, Contract, ContractWrapper, Executor};
+
+    use crate::msg::{QueryMsg, ValueResp};
+    use crate::{execute, instantiate, query};
+
+    fn counting_contract() -> Box<dyn Contract<Empty>> {
+        let contract = ContractWrapper::new(execute, instantiate, query);
+        Box::new(contract)
+    }
+
+    #[test]
+    fn query_value() {
+        let mut app = App::default();
+
+        let contract_id = app.store_code(counting_contract());
+
+        let contract_addr = app
+            .instantiate_contract(
+                contract_id,
+                Addr::unchecked("sender"),
+                &Empty {},
+                &[],
+                "Counting contract",
+                None,
+            )
+            .unwrap();
+
+        let resp: ValueResp = app
+            .wrap()
+            .query_wasm_smart(contract_addr, &QueryMsg::Value {})
+            .unwrap();
+
+        assert_eq!(resp, ValueResp { value: 0 });
     }
 }
